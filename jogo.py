451e5d042e3af01921a10975323a84ba244396d0
JoninhasC <jonas149.correia@gmail.com>
2025-09-26 15:13:59 -0300

    atualização

--- jogo.py
+++ jogo.py
@@ -1,5 +1,4 @@
 # Jogo simples em Python
 print("Bem-vindo ao jogo!")
 print("Este é um arquivo de exemplo para o Git.")
-print("Repositório: Teste-para-tutores")
-print("Usuário: JoninhasC")
+print("Este é um teste para o Git.")
